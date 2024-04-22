e7b3e211e0111d6553bf9bab30794ddab697fb44
Syed Zain Anwer <zainanwer24@gmail.com>
2024-04-22 23:43:11 +0200

    create routes for the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,39 @@
+import { BrowserRouter, Routes, Navigate, Route } from "react-router-dom";
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import ProtectedRoutes from './components/ProtectedRoutes';
+
+const Logout = () => {
+  localStorage.clear();
+  return <Navigate to="/login" />
+}
+
+const RegisterAndLogout = () => {
+  localStorage.clear();
+  return <Register />
+}
+
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
+      <Routes>
+        <Route 
+          path="/"
+          element={
+            <ProtectedRoutes>
+              <Home />
+            </ProtectedRoutes>
+          }
+        />
 
-    </>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<RegisterAndLogout />}/>
+        <Route path="*" element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
